f2542d2556701fdfc43a75b19b7643d64ccb2b0b
Muhammad Sannan <sannansnn@gmail.com>
2025-11-05 02:07:26 -0800

    Almost There Now

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -9,16 +9,14 @@ import "swiper/css/effect-cards";
 // Import required modules
 import { EffectCards } from "swiper/modules";
 
-import "./styles.css";
+import "./Styles.css";
 
 export default function Slider() {
   const slides = [
+    "https://images.unsplash.com/photo-1622782914767-404fb9ab3f57?auto=format&fit=crop&q=60&w=400",
     "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&q=60&w=400",
     "https://images.unsplash.com/photo-1546054454-aa26e2b734c7?auto=format&fit=crop&q=60&w=400",
     "https://plus.unsplash.com/premium_photo-1681666713741-3e307db44f68?auto=format&fit=crop&q=60&w=400",
-    "https://images.unsplash.com/photo-1622782914767-404fb9ab3f57?auto=format&fit=crop&q=60&w=400",
-    "https://images.unsplash.com/photo-1622782914767-404fb9ab3f57?auto=format&fit=crop&q=60&w=400",
-    "https://images.unsplash.com/photo-1622782914767-404fb9ab3f57?auto=format&fit=crop&q=60&w=400",
     "https://images.unsplash.com/photo-1622782914767-404fb9ab3f57?auto=format&fit=crop&q=60&w=400",
   ];
 
